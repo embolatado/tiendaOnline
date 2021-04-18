619f83dea99c1cd4775e5ee57c3aa39c93abbd3b
embolatado <embolatado@gmail.com>
2021-04-18 18:13:16 -0500

    class panelPedidos and filters for date

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -9,7 +9,13 @@ class panelClientes(admin.ModelAdmin):
 class panelArticulos(admin.ModelAdmin):
     list_filter=('seccion', )
 
+# FILTRO FECHA
+class panelPedidos(admin.ModelAdmin):
+    list_display=("numero", "fecha")
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+
 # Register your models here Y LAS VISTAS PERSONALIZADAS DEL /ADMIN
 admin.site.register(Clientes, panelClientes)
 admin.site.register(Articulos, panelArticulos)
-admin.site.register(Pedidos)
+admin.site.register(Pedidos, panelPedidos)

8718c6ab5692f189b9b2ec931d188c431fe5c216
embolatado <embolatado@gmail.com>
2021-04-18 17:07:31 -0500

    Search bar in personalized panel admin view added

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -4,7 +4,7 @@ from gestionPedidos.models import Clientes, Articulos, Pedidos
 # CREAR VISTAS PERSONALIZADAS PARA EL PANEL DE ADMINISTRACIÓN.
 class panelClientes(admin.ModelAdmin):
     list_display=('nombre', 'email', 'tfno')
-
+    search_fields = ("nombre", "tfno")
 
 # Register your models here Y LAS VISTAS PERSONALIZADAS DEL /ADMIN
 admin.site.register(Clientes, panelClientes)

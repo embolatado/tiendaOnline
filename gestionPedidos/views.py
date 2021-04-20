81a01ca6831be330134cf87965b101f4e9217852
embolatado <embolatado@gmail.com>
2021-04-20 07:14:33 -0500

    Limitar caracteres búsqueda que van a servidor

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -10,16 +10,21 @@ def busqueda_productos(request):
 
 # VISTA DEL RESULTADO + CONTROL DE BÚSQUEDA STRING VACÍO
 def resultado_busqueda(request):
-    
     if request.GET["prd"]:
         # mensaje = "Artículo buscado: %r" % request.GET["prd"]
-
+        
         producto = request.GET["prd"]
+        
+        # LIMITAR NÚMERO CARACTERES QUE SE ENVÍAN AL SERVIDOR
+        if len(producto) > 30:
+            mensaje = "Texto de búsqueda demasiado largo."
 
-        articulos = Articulos.objects.filter(nombre__icontains = producto)
+        else:
+            articulos = Articulos.objects.filter(nombre__icontains = producto)
 
-        return render(request, "resultado_busqueda.html", {"arts": articulos, "consulta": producto})
+            return render(request, "resultado_busqueda.html", {"arts": articulos, "consulta": producto})
 
     else:
         mensaje = "No has escrito nada."
+    
     return HttpResponse(mensaje)

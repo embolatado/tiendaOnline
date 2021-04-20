23d551802101ed0f5d04b5061d6fa4517374b709
embolatado <embolatado@gmail.com>
2021-04-19 23:14:51 -0500

    Consulta y vista de resultados

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import Articulos
 
 # Create your views here.
 
@@ -7,11 +8,18 @@ from django.http import HttpResponse
 def busqueda_productos(request):
     return render(request, "buscar_producto.html")
 
-# VISTA DEL RESULTADO
+# VISTA DEL RESULTADO + CONTROL DE BÚSQUEDA STRING VACÍO
 def resultado_busqueda(request):
     
     if request.GET["prd"]:
-        mensaje = "Artículo buscado: %r" % request.GET["prd"]
+        # mensaje = "Artículo buscado: %r" % request.GET["prd"]
+
+        producto = request.GET["prd"]
+
+        articulos = Articulos.objects.filter(nombre__icontains = producto)
+
+        return render(request, "resultado_busqueda.html", {"arts": articulos, "consulta": producto})
+
     else:
         mensaje = "No has escrito nada."
     return HttpResponse(mensaje)
